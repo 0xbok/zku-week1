65ad374d22fba2bf7a00b0c2c6f3dd7aaf5b3d2f
0xbok <1689531+0xbok@users.noreply.github.com>
2022-05-10 00:18:08 +0530

    week 1 assignment

--- Q2/test/test.js
+++ Q2/test/test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const fs = require("fs");
-const { groth16 } = require("snarkjs");
+const { groth16, plonk } = require("snarkjs");
 
 function unstringifyBigInts(o) {
     if ((typeof(o) == "string") && (/^[0-9]+$/.test(o) ))  {
@@ -35,21 +35,30 @@ describe("HelloWorld", function () {
 
     it("Should return true for correct proof", async function () {
         //[assignment] Add comments to explain what each line is doing
+        // generate a proof and the output result using from inputs and compiled circuit, and keys generated for groth16
         const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2"}, "contracts/circuits/HelloWorld/HelloWorld_js/HelloWorld.wasm","contracts/circuits/HelloWorld/circuit_final.zkey");
 
+        // print the first public output of the circuit evaluation, it should be equal to 1x2 = 3
         console.log('1x2 =',publicSignals[0]);
 
+        // next we check if the verifier is able to verify the proof generated above
+        // convert the data type of `publicSignals` to BigInt for Solidity compatiblity
         const editedPublicSignals = unstringifyBigInts(publicSignals);
+        // convert the data type of `proof` to BigInt for Solidity compatiblity
         const editedProof = unstringifyBigInts(proof);
+        // create the calldata that are passed to the smart contract verifier function
         const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals);
-    
+
+        // now deconstruct the calldata so that we can extract the arguments required for `verifyProof` function.
         const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
-    
+
+        // extract arguments
         const a = [argv[0], argv[1]];
         const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
         const c = [argv[6], argv[7]];
         const Input = argv.slice(8);
 
+        // assert that the verifier is able to verify the proof
         expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
     });
     it("Should return false for invalid proof", async function () {
@@ -66,13 +75,46 @@ describe("Multiplier3 with Groth16", function () {
 
     beforeEach(async function () {
         //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("Multiplier3Verifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
     });
 
     it("Should return true for correct proof", async function () {
         //[assignment] insert your script here
+        const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2","c":"3"}, "contracts/circuits/Multiplier3/Multiplier3_js/Multiplier3.wasm","contracts/circuits/Multiplier3/circuit_final.zkey");
+
+        // print the first public output of the circuit evaluation, it should be equal to 1x2 = 3
+        console.log('1x2x3 =',publicSignals[0]);
+
+        // next we check if the verifier is able to verify the proof generated above
+        // convert the data type of `publicSignals` to BigInt for Solidity compatiblity
+        const editedPublicSignals = unstringifyBigInts(publicSignals);
+        // convert the data type of `proof` json values to BigInt for Solidity compatiblity
+        const editedProof = unstringifyBigInts(proof);
+        // create the calldata that are passed to the smart contract verifier function
+        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals);
+
+        // now deconstruct the calldata so that we can extract the arguments required for `verifyProof` function.
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+
+        // extract arguments
+        const a = [argv[0], argv[1]];
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
+        const c = [argv[6], argv[7]];
+        const Input = argv.slice(8);
+
+        // assert that the verifier is able to verify the proof
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
+
     });
     it("Should return false for invalid proof", async function () {
         //[assignment] insert your script here
+        let a = [0, 0];
+        let b = [[0, 0], [0, 0]];
+        let c = [0, 0];
+        let d = [0]
+        expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
     });
 });
 
@@ -81,12 +123,40 @@ describe("Multiplier3 with PLONK", function () {
 
     beforeEach(async function () {
         //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("Multiplier3PlonkVerifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
     });
 
     it("Should return true for correct proof", async function () {
         //[assignment] insert your script here
+        const { proof, publicSignals } = await plonk.fullProve({"a":"1","b":"2","c":"3"}, "contracts/circuits/Multiplier3_plonk/Multiplier3_js/Multiplier3.wasm","contracts/circuits/Multiplier3_plonk/circuit_final.zkey");
+
+        // print the first public output of the circuit evaluation, it should be equal to 1x2 = 3
+        console.log('1x2x3 =',publicSignals[0]);
+
+        // next we check if the verifier is able to verify the proof generated above
+        // convert the data type of `publicSignals` to BigInt for Solidity compatiblity
+        const editedPublicSignals = unstringifyBigInts(publicSignals);
+        // convert the data type of `proof` to BigInt for Solidity compatiblity
+        const editedProof = unstringifyBigInts(proof);
+        // create the calldata that are passed to the smart contract verifier function
+        const calldata = await plonk.exportSolidityCallData(editedProof, editedPublicSignals);
+
+        // now deconstruct the calldata so that we can extract the arguments required for `verifyProof` function.
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',');
+        // extract arguments
+        const a = argv[0]
+        const Input = argv.slice(1)
+
+        // assert that the verifier is able to verify the proof
+        expect(await verifier.verifyProof(a, Input)).to.be.true;
+
     });
     it("Should return false for invalid proof", async function () {
         //[assignment] insert your script here
+        let a = 0x0;
+        let b = [0];
+        expect(await verifier.verifyProof(a, b)).to.be.false;
     });
 });
--- Q3/contracts/bonus/SystemOfEquations.circom
+++ Q3/contracts/bonus/SystemOfEquations.circom
@@ -1,7 +1,8 @@
 pragma circom 2.0.0;
 
 include "../../node_modules/circomlib/circuits/comparators.circom";
-include ""; // hint: you can use more than one templates in circomlib-matrix to help you
+include "../../node_modules/circomlib-matrix/circuits/matMul.circom"; // hint: you can use more than one templates in circomlib-matrix to help you
+include "../../node_modules/circomlib-matrix/circuits/matSub.circom";
 
 template SystemOfEquations(n) { // n is the number of variables in the system of equations
     signal input x[n]; // this is the solution to the system of equations
@@ -10,6 +11,36 @@ template SystemOfEquations(n) { // n is the number of variables in the system of
     signal output out; // 1 for correct solution, 0 for incorrect solution
 
     // [bonus] insert your code here
+    component matmul = matMul(n,n,1);
+    for (var i=0; i < n; i++) {
+        for (var j=0; j < n; j++) {
+            matmul.a[i][j] <== A[i][j];
+        }
+        matmul.b[i][0] <== x[i];
+    }
+
+    component matsub = matSub(n,1);
+    for (var i=0; i < n; i++) {
+        matsub.a[i][0] <== b[i];
+        matsub.b[i][0] <== matmul.out[i][0];
+    }
+
+    component zero[n+1];
+    signal sum[n];
+
+    zero[0] = IsZero();
+    zero[0].in <== matsub.out[0][0];
+    sum[0] <== zero[0].out;
+    for (var i=1; i < n; i++) {
+        zero[i] = IsZero();
+        zero[i].in <== matsub.out[i][0];
+        sum[i] <== sum[i-1]+zero[i].out;
+    }
+
+    zero[n] = IsZero();
+    zero[n].in <== sum[n-1] - n;
+
+    out <== zero[n].out;
 }
 
 component main {public [A, b]} = SystemOfEquations(3);
